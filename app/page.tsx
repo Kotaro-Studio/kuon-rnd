50b0f92033320b6411e625eb9f45e89611d228d2
kotaro <cat@kotarohattori.com>
2026-03-21 10:54:49 +0900

    正しい階層構造で再アップロード

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>空音開発へようこそ！</h1>
+      <p>ついに自分の手で、Webサイトの第一歩を踏み出しました。</p>
+    </main>
+  );
+}
